00c14498fa4d61e5572b581e0774bee5401d192c
basegiojunior <basegiojunior@gmail.com>
2022-08-05 22:54:24 -0300

    first commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './Home.style';
+
+export const Home: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+};
