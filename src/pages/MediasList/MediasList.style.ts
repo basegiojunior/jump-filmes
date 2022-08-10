05cab9f0acdd16fdb6518ce1a0df1aaed999a32d
basegiojunior <basegiojunior@gmail.com>
2022-08-10 12:31:12 -0300

    fix: medias list page background

--- src/pages/MediasList/MediasList.style.ts
+++ src/pages/MediasList/MediasList.style.ts
@@ -1,9 +1,7 @@
-import { colors } from 'src/styles/colors';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${colors.background};
 `;
 
 export const ActivityIndicatorContainer = styled.View`
